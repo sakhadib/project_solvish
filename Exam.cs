50b2d6193aededc24b5aa0395688d5ff89e0c13d
Adib Sakhawat <adibsakhawat@iut-dhaka.edu>
2023-03-11 01:56:21 +0600

    Added Random number generator at utility class.

--- Exam.cs
+++ Exam.cs
@@ -8,6 +8,20 @@ namespace Solvish
 {
     internal class Exam
     {
-        List<Question> Questions = new List<Question>();
+        public List<Question> Questions = new List<Question>();
+        public int right_ans_ct { get; set; }
+        public int wrong_ans_ct { get; set; }
+        public int skipped_ct { get; set; }
+        public double point { get; set; }
+
+        public Exam()
+        {
+
+        }
+
+        public void add_question(int code, int count)
+        {
+
+        }
     }
 }
--- Utility.cs
+++ Utility.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace Solvish
 {
-    internal class Utility
+    class Utility
     {
         //Random Number Generator
+        //just call with an int array and it will return the desired value.
+        private Random random = new Random(); // create an instance of the Random class
+
+        public int[] GenerateRandomNumbers(int count, int min, int max)
+        {
+            int[] numbers = new int[count]; // create an array to store the random numbers
+
+            for (int i = 0; i < count; i++)
+            {
+                numbers[i] = random.Next(min, max); // generate a random number between min and max
+            }
+
+            return numbers;
+        }
+
+
         //Other Necessary Functions
-        
+
     }
 }
