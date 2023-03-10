5e5fe44b2b43a0c3fc4e3a7569fb497e17da5aad
KHALID HASAN ADOR <khalidhasan@iut-dhaka.edu>
2023-03-11 02:39:56 +0600

    Added Chapter Class

---
+++ Chapter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solvish
+{
+    internal class Chapter
+    {
+        public string name {  get; set; }
+        public int ChapterNumber { get; set; }
+        
+        List<Question> questions = new List<Question>();
+
+        public Chapter (string name, int number) 
+        {
+            this.name = name;
+            ChapterNumber = number;
+        }
+    }
+}
