734c11a28842bae3f278fbcfb2ebee904a77bbae
KHALID HASAN ADOR <khalidhasan@iut-dhaka.edu>
2023-03-11 02:55:18 +0600

    Edited Student Class

--- student.cs
+++ student.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace Solvish
 {
     internal class student
     {
+        List<Exam> exams = new List<Exam>();
         public string fname { get; set; }
         public string lname { get; set; }
         public string phone { get; set; }
@@ -16,6 +18,10 @@ namespace Solvish
         public string cclass { get; set; } // as class is not a usable variable
 
         public string password { get; set; }
+
+        public int totalRight { get; set; }
+        public int totalWrong { get; set; }
+        public double totalPoint { get; set; }
 
         public student (string fname, string lname, string phone, string email, string school, string cclass, string password)
         {
@@ -26,6 +32,13 @@ namespace Solvish
             this.school = school;
             this.cclass = cclass;
             this.password = password;
+        }
+
+        public void addexam(Exam e)
+        {
+            totalRight += e.right_ans_ct;
+            totalWrong += e.wrong_ans_ct;
+            totalPoint += e.point;
         }
     }
 }
