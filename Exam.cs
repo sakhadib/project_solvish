50a40ab51d55171460ce98a319e66ad2130e6d77
KHALID HASAN ADOR <khalidhasan@iut-dhaka.edu>
2023-03-10 23:19:20 +0600

    Added Necessary Classes

---
+++ Exam.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solvish
+{
+    internal class Exam
+    {
+        List<Question> Questions = new List<Question>();
+    }
+}
---
+++ Question.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solvish
+{
+    internal class Question
+    {
+        public string statement { get; set; }
+        public string Option1 { get; set; }
+        public string Option2 { get; set; }
+        public string Option3 { get; set; }
+        public string Option4 { get; set; }
+        public string CorrectAnswer { get; set; }
+
+        public Question(string statement, string option1, string option2, string option3, string option4, string correctAnswer)
+        {
+            this.statement = statement;
+            Option1 = option1;
+            Option2 = option2;
+            Option3 = option3;
+            Option4 = option4;
+            CorrectAnswer = correctAnswer;
+        }
+    }
+}
