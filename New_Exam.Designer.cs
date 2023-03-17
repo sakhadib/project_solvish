813570b4a33e397f452747538b7bc1ea4d60743a
KHALID HASAN ADOR <khalidhasan@iut-dhaka.edu>
2023-03-17 21:12:45 +0600

    Modified New-Exam

--- New_Exam.Designer.cs
+++ New_Exam.Designer.cs
@@ -34,6 +34,10 @@
             this.label2 = new System.Windows.Forms.Label();
             this.tb_minute = new System.Windows.Forms.TextBox();
             this.StartExambutton = new System.Windows.Forms.Button();
+            this.tb_subject = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_chapter = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // tb_hour
@@ -41,7 +45,7 @@
             this.tb_hour.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(5)))), ((int)(((byte)(13)))));
             this.tb_hour.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tb_hour.ForeColor = System.Drawing.SystemColors.Info;
-            this.tb_hour.Location = new System.Drawing.Point(197, 20);
+            this.tb_hour.Location = new System.Drawing.Point(189, 192);
             this.tb_hour.Name = "tb_hour";
             this.tb_hour.Size = new System.Drawing.Size(63, 40);
             this.tb_hour.TabIndex = 10;
@@ -51,7 +55,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label4.Location = new System.Drawing.Point(12, 31);
+            this.label4.Location = new System.Drawing.Point(4, 203);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(73, 29);
             this.label4.TabIndex = 9;
@@ -62,7 +66,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(118, 31);
+            this.label1.Location = new System.Drawing.Point(110, 203);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(73, 29);
             this.label1.TabIndex = 11;
@@ -73,7 +77,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label2.Location = new System.Drawing.Point(287, 31);
+            this.label2.Location = new System.Drawing.Point(279, 203);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(97, 29);
             this.label2.TabIndex = 12;
@@ -84,7 +88,7 @@
             this.tb_minute.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(5)))), ((int)(((byte)(13)))));
             this.tb_minute.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tb_minute.ForeColor = System.Drawing.SystemColors.Info;
-            this.tb_minute.Location = new System.Drawing.Point(390, 20);
+            this.tb_minute.Location = new System.Drawing.Point(382, 192);
             this.tb_minute.Name = "tb_minute";
             this.tb_minute.Size = new System.Drawing.Size(63, 40);
             this.tb_minute.TabIndex = 13;
@@ -98,7 +102,7 @@
             this.StartExambutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.StartExambutton.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.StartExambutton.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.StartExambutton.Location = new System.Drawing.Point(155, 98);
+            this.StartExambutton.Location = new System.Drawing.Point(132, 261);
             this.StartExambutton.Name = "StartExambutton";
             this.StartExambutton.Size = new System.Drawing.Size(261, 51);
             this.StartExambutton.TabIndex = 14;
@@ -106,12 +110,58 @@
             this.StartExambutton.UseVisualStyleBackColor = false;
             this.StartExambutton.Click += new System.EventHandler(this.StartExambutton_Click);
             // 
+            // tb_subject
+            // 
+            this.tb_subject.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(5)))), ((int)(((byte)(13)))));
+            this.tb_subject.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_subject.ForeColor = System.Drawing.SystemColors.Info;
+            this.tb_subject.Location = new System.Drawing.Point(225, 12);
+            this.tb_subject.Name = "tb_subject";
+            this.tb_subject.Size = new System.Drawing.Size(220, 40);
+            this.tb_subject.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(7, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 29);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Subject";
+            // 
+            // tb_chapter
+            // 
+            this.tb_chapter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(5)))), ((int)(((byte)(13)))));
+            this.tb_chapter.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_chapter.ForeColor = System.Drawing.SystemColors.Info;
+            this.tb_chapter.Location = new System.Drawing.Point(225, 67);
+            this.tb_chapter.Name = "tb_chapter";
+            this.tb_chapter.Size = new System.Drawing.Size(220, 40);
+            this.tb_chapter.TabIndex = 18;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Location = new System.Drawing.Point(7, 73);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 29);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Chapter";
+            // 
             // New_Exam
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(756, 324);
+            this.Controls.Add(this.tb_chapter);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tb_subject);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.StartExambutton);
             this.Controls.Add(this.tb_minute);
             this.Controls.Add(this.label2);
@@ -133,5 +183,9 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox tb_minute;
         private System.Windows.Forms.Button StartExambutton;
+        private System.Windows.Forms.TextBox tb_subject;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_chapter;
+        private System.Windows.Forms.Label label5;
     }
 }
