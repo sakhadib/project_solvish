b5bb373670ae29052babe6b7da5fcd4a753fba0c
Adib Sakhawat <adibsakhawat@iut-dhaka.edu>
2023-03-26 01:00:14 +0600

    finalized login form design

--- LOGIN_FORM.Designer.cs
+++ LOGIN_FORM.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login_form));
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.uname_tb = new System.Windows.Forms.TextBox();
@@ -39,47 +38,37 @@
             this.button3 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label4 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Calibri", 90F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(47, 411);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(478, 146);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "SOLVISH";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.label2.Location = new System.Drawing.Point(742, 233);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
+            this.label2.Location = new System.Drawing.Point(164, 350);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(132, 29);
+            this.label2.Size = new System.Drawing.Size(111, 29);
             this.label2.TabIndex = 1;
-            this.label2.Text = "USER NAME";
+            this.label2.Text = "username";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.label3.Location = new System.Drawing.Point(747, 341);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
+            this.label3.Location = new System.Drawing.Point(165, 466);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(124, 29);
+            this.label3.Size = new System.Drawing.Size(107, 29);
             this.label3.TabIndex = 1;
-            this.label3.Text = "PASSWORD";
+            this.label3.Text = "password";
             // 
             // uname_tb
             // 
@@ -87,8 +76,9 @@
             this.uname_tb.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.uname_tb.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.uname_tb.ForeColor = System.Drawing.SystemColors.Info;
-            this.uname_tb.Location = new System.Drawing.Point(647, 172);
+            this.uname_tb.Location = new System.Drawing.Point(59, 297);
             this.uname_tb.Margin = new System.Windows.Forms.Padding(10);
+            this.uname_tb.MaxLength = 15;
             this.uname_tb.Name = "uname_tb";
             this.uname_tb.Size = new System.Drawing.Size(321, 40);
             this.uname_tb.TabIndex = 2;
@@ -100,7 +90,8 @@
             this.pass_tb.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.pass_tb.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.pass_tb.ForeColor = System.Drawing.SystemColors.Info;
-            this.pass_tb.Location = new System.Drawing.Point(647, 288);
+            this.pass_tb.Location = new System.Drawing.Point(18, 10);
+            this.pass_tb.MaxLength = 12;
             this.pass_tb.Name = "pass_tb";
             this.pass_tb.Size = new System.Drawing.Size(321, 40);
             this.pass_tb.TabIndex = 2;
@@ -117,9 +108,9 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.button1.Location = new System.Drawing.Point(693, 390);
+            this.button1.Location = new System.Drawing.Point(122, 531);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(243, 51);
+            this.button1.Size = new System.Drawing.Size(187, 51);
             this.button1.TabIndex = 4;
             this.button1.Text = "LOG IN";
             this.button1.UseVisualStyleBackColor = false;
@@ -130,7 +121,7 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label6.Location = new System.Drawing.Point(642, 502);
+            this.label6.Location = new System.Drawing.Point(54, 640);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(250, 29);
             this.label6.TabIndex = 1;
@@ -156,7 +147,7 @@
             // 
             this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(1038, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(381, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(48, 51);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -168,23 +159,13 @@
             // 
             this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox3.Image = global::Solvish.Properties.Resources.eye;
-            this.pictureBox3.Location = new System.Drawing.Point(995, 285);
+            this.pictureBox3.Location = new System.Drawing.Point(307, 6);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(48, 44);
+            this.pictureBox3.Size = new System.Drawing.Size(40, 44);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox3.TabIndex = 0;
             this.pictureBox3.TabStop = false;
             this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = global::Solvish.Properties.Resources.main_logo;
-            this.pictureBox2.Location = new System.Drawing.Point(97, 95);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(367, 384);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 0;
-            this.pictureBox2.TabStop = false;
             // 
             // label4
             // 
@@ -192,7 +173,7 @@
             this.label4.Cursor = System.Windows.Forms.Cursors.Hand;
             this.label4.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(172)))), ((int)(((byte)(255)))));
-            this.label4.Location = new System.Drawing.Point(898, 502);
+            this.label4.Location = new System.Drawing.Point(310, 640);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(87, 29);
             this.label4.TabIndex = 1;
@@ -202,7 +183,7 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
-            this.panel1.Location = new System.Drawing.Point(630, 162);
+            this.panel1.Location = new System.Drawing.Point(42, 287);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(355, 60);
             this.panel1.TabIndex = 5;
@@ -210,28 +191,37 @@
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
-            this.panel2.Location = new System.Drawing.Point(630, 277);
+            this.panel2.Controls.Add(this.pictureBox3);
+            this.panel2.Controls.Add(this.pass_tb);
+            this.panel2.Location = new System.Drawing.Point(42, 402);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(355, 60);
             this.panel2.TabIndex = 5;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Solvish.Properties.Resources.Artboard_3;
+            this.pictureBox2.Location = new System.Drawing.Point(42, 86);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(367, 117);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
             // 
             // Login_form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(5)))), ((int)(((byte)(13)))));
-            this.ClientSize = new System.Drawing.Size(1098, 646);
+            this.ClientSize = new System.Drawing.Size(441, 741);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.pass_tb);
             this.Controls.Add(this.uname_tb);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
@@ -242,6 +232,8 @@
             this.Load += new System.EventHandler(this.form_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -249,9 +241,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox uname_tb;
@@ -264,5 +253,6 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
