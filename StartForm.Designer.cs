0f0de23b3ae180f96026b73c8aa76fe0453141de
sakhasib <adibsakhawat@iut-dhaka.edu>
2023-05-09 14:08:58 +0600

    Edited Startform

--- StartForm.Designer.cs
+++ StartForm.Designer.cs
@@ -43,9 +43,10 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = global::Solvish.Properties.Resources.CROSS;
-            this.pictureBox2.Location = new System.Drawing.Point(371, 12);
+            this.pictureBox2.Location = new System.Drawing.Point(495, 15);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(51, 55);
+            this.pictureBox2.Size = new System.Drawing.Size(68, 68);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 4;
             this.pictureBox2.TabStop = false;
@@ -56,9 +57,10 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label2.Location = new System.Drawing.Point(176, 112);
+            this.label2.Location = new System.Drawing.Point(235, 161);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(185, 43);
+            this.label2.Size = new System.Drawing.Size(233, 54);
             this.label2.TabIndex = 16;
             this.label2.Text = "SOLVISH";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -66,9 +68,10 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Solvish.Properties.Resources.Artboard_1_copy_2x;
-            this.pictureBox1.Location = new System.Drawing.Point(-1, 53);
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 88);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(264, 164);
+            this.pictureBox1.Size = new System.Drawing.Size(352, 202);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 17;
             this.pictureBox1.TabStop = false;
@@ -76,31 +79,34 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(-6, 238);
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label1.Location = new System.Drawing.Point(-6, 320);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(367, 43);
+            this.label1.Size = new System.Drawing.Size(227, 27);
             this.label1.TabIndex = 18;
             this.label1.Text = "Hold Your Horses...";
             // 
             // PercentageLabel
             // 
             this.PercentageLabel.AutoSize = true;
-            this.PercentageLabel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PercentageLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.PercentageLabel.Location = new System.Drawing.Point(349, 250);
+            this.PercentageLabel.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PercentageLabel.ForeColor = System.Drawing.Color.Gray;
+            this.PercentageLabel.Location = new System.Drawing.Point(490, 321);
+            this.PercentageLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.PercentageLabel.Name = "PercentageLabel";
-            this.PercentageLabel.Size = new System.Drawing.Size(146, 28);
+            this.PercentageLabel.Size = new System.Drawing.Size(105, 27);
             this.PercentageLabel.TabIndex = 19;
             this.PercentageLabel.Text = "Percentage";
             // 
             // progressBar1
             // 
             this.progressBar1.ForeColor = System.Drawing.SystemColors.Control;
-            this.progressBar1.Location = new System.Drawing.Point(-1, 285);
+            this.progressBar1.Location = new System.Drawing.Point(-1, 351);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(437, 18);
+            this.progressBar1.Size = new System.Drawing.Size(583, 22);
             this.progressBar1.TabIndex = 20;
             this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
             // 
@@ -111,10 +117,10 @@
             // 
             // StartForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(436, 303);
+            this.ClientSize = new System.Drawing.Size(583, 373);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.PercentageLabel);
             this.Controls.Add(this.label1);
@@ -122,6 +128,7 @@
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "StartForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
